dd7214ea926524d58acc68c8e4c0a9f220e0d331
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-09-14 13:21:51 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,30 +1,54 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Statistics.Test
 {
     public class StatsUnitTest
     {
+        
         [Fact]
-        public void ReportsAverageMinMax()
+        public void ReportsCalulatedStatisticsForAListOfNumbers()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+                new List<double>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Min - 1.5) <= epsilon);
         }
+        [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
-            //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+                new List<double>{});
+            Assert.True(double.IsNaN(computedStats.Average));
+            Assert.True(double.IsNaN(computedStats.Max));
+            Assert.True(double.IsNaN(computedStats.Min));
+    
         }
+        [Fact]
+        public void WhenCallerInvokesAListOfNumbersHavingNaNThenStatsOfRemainingAreReturnedAndListSizeRemainsUnchanged()
+        {   List<double> ListWithNanValues = new List<double>() {1.8,double.NaN,6.9,1.1};
+            int count=ListWithNanValues.Count;
+            var statsComputer = new StatsComputer();
+            var computedStats = statsComputer.CalculateStatistics(
+                ListWithNanValues);
+                float epsilon = 0.001F;
+            Assert.True(Math.Abs(computedStats.Average - 3.26666667) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Max - 6.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Min - 1.1) <= epsilon);
+            Assert.True(ListWithNanValues.Count== count);
+          
+            
+        }
+    
+        
     }
 }
